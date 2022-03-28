c02c88faecfa05cd7815b694ae978a66e799da94
rst <rst5995@gmail.com>
2022-03-28 19:31:35 +0300

    sonar fix

--- src/test/java/com/bmxstore/grind_store/web/CurrencyControllerTest.java
+++ src/test/java/com/bmxstore/grind_store/web/CurrencyControllerTest.java
@@ -8,11 +8,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
-
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
 @SpringBootTest
 @AutoConfigureMockMvc
